f7148029272377fcd25b8e559f3913e3c52651c3
Alan Gutierrez <alan@prettyrobots.com>
2010-11-17 19:35:56 -0500

    Added provisioned flag.

--- ddl/0001_create_user.sql
+++ ddl/0001_create_user.sql
@@ -14,6 +14,7 @@ CREATE TABLE Account (
     id              INTEGER NOT NULL AUTO_INCREMENT,
     email           VARCHAR(255) NOT NULL,
     sshKey          TEXT,
+    provisioned     INTEGER NOT NULL,
     modified        TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     created         TIMESTAMP DEFAULT 0,
     PRIMARY KEY (id)
@@ -33,6 +34,7 @@ CREATE TABLE ApplicationLocalUser (
     machineId       INTEGER NOT NULL,
     localUserId     INTEGER NOT NULL,
     applicationId   INTEGER NOT NULL,
+    provisioned     INTEGER NOT NULL,
     modified        TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     created         TIMESTAMP NOT NULL DEFAULT 0,
     PRIMARY KEY (id)
