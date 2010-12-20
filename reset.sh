88ed02506a5a33e6eade74e161b7d6496d9e9de6
Alan Gutierrez <alan@prettyrobots.com>
2010-12-20 08:32:24 -0500

    Moved reset script into system.

--- reset.sh
+++
@@ -1,4 +0,0 @@
-echo "drop database puppy" | mysql -u root
-echo "create database puppy" | mysql -u root
-mysql -u root puppy < ddl/0001_create_user.sql 
-mysql -u root puppy < ddl/0002_insert_account.sql 
