efd9cd8cdd5588f441aa7f25f9f163114a196120
Alan Gutierrez <alan@prettyrobots.com>
2010-12-24 02:57:28 -0500

    Transition swallows stack trackes.
     * Swallowing stack traces and writing them to the syslog instead of spewing
       them out to the user. Don't want to reveal passwords.

--- bin/public_transition.js
+++ bin/public_transition.js
@@ -1,5 +1,6 @@
 var spawn = require("child_process").spawn;
 var body = "";
+var syslog = new (require("common/syslog").Syslog)({ tag: "public_transition", pid: true })
 
 var stdin = process.openStdin();
 stdin.on("data", function (chunk) {
@@ -20,8 +21,16 @@ stdin.on("end", function () {
   command.unshift("puppy");
   command.unshift("-u");
 
+  var stderr = []
   public = spawn("sudo", command);
   public.stdout.on("data", function (chunk) { process.stdout.write(chunk.toString()) });
-  public.stderr.on("data", function (chunk) { process.stdout.write(chunk.toString()) });
-  public.on("exit", function (code) { process.stdout.write(code); process.exit(code) })
+  public.stderr.on("data", function (chunk) { stderr.push(chunk.toString()) });
+  public.on("exit", function (code) {
+    if (code || stderr.length) {
+      syslog.send( "err",
+        "Recieved unexpected error messages with exit code #{code}.",
+        { stderr: stderr.join(""), code: code });
+    }
+    process.exit(code);
+  })
 });
