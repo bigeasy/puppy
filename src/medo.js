8309d8ac43b5a130b90c3f5e1999af3b5bb07e7d
Alan Gutierrez <alan@prettyrobots.com>
2011-03-20 09:27:28 -0400

    Fixed `medo.js` after running for first time.

--- src/medo.js
+++ src/medo.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
 var spawn = require("child_process").spawn
-var program = "/puppy/worker/sbin/" + __filename.replace(/^.*\/(.*).coffee$/, "$1");
-var run = spawn(program, process.argv.slice(1), { customFds: [ 0, 1, 2 ] });
-sudo.on("exit", function (code) { process.exit(code) });
+var program = "/puppy/worker/sbin/" + __filename.replace(/^.*\/(.*)$/, "$1");
+spawn(program, process.argv.slice(1), { customFds: [ 0, 1, 2 ] }).on("exit", function (code) { process.exit(code) });
