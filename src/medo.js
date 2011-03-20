3c9daff112d0b1f73d93579e1b1c444b4da7d1c2
Alan Gutierrez <alan@prettyrobots.com>
2011-03-20 03:05:13 -0400

    Made sudo and medo minimal Node.js programs.
     * These programs no longer load the common library.

--- src/medo.js
+++ src/medo.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
-require.paths.unshift("/puppy/common/lib/node");
-var program = __filename.replace(/^.*\/(.*).coffee$/, "$1");
-require("system").medo("/puppy/worker/sbin/" + program, process.argv.slice(2));
+var spawn = require("child_process").spawn
+var program = "/puppy/worker/sbin/" + __filename.replace(/^.*\/(.*).coffee$/, "$1");
+var run = spawn(program, process.argv.slice(1), { customFds: [ 0, 1, 2 ] });
+sudo.on("exit", function (code) { process.exit(code) });
--- src/sudo.js
+++ src/sudo.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
-require.paths.unshift("/puppy/common/lib/node");
-var program = __filename.replace(/^.*\/(.*).coffee$/, "$1");
-require("common").sudo("/puppy/worker/sbin/" + program, process.argv.slice(2));
+var spawn = require("child_process").spawn
+var program = "/puppy/worker/sbin/" + __filename.replace(/^.*\/(.*)$/, "$1");
+var parameters = process.argv.slice(2);
+parameters.unshift(program);
+var sudo = spawn("/usr/bin/sudo", parameters, { customFds: [ 0, 1, 2 ] });
+sudo.on("exit", function (code) { process.exit(code) });
