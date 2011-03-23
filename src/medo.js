97d26faa72d7fb9f255ed429f779cb08a979d0b6
Alan Gutierrez <alan@prettyrobots.com>
2011-03-23 06:42:46 -0400

    Fixed bug with argument passing in `medo.js`.

--- src/medo.js
+++ src/medo.js
@@ -2,4 +2,4 @@
 
 var spawn = require("child_process").spawn
 var program = "/puppy/worker/sbin/" + __filename.replace(/^.*\/(.*)$/, "$1");
-spawn(program, process.argv.slice(1), { customFds: [ 0, 1, 2 ] }).on("exit", function (code) { process.exit(code) });
+spawn(program, process.argv.slice(2), { customFds: [ 0, 1, 2 ] }).on("exit", function (code) { process.exit(code) });
