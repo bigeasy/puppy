a9ae0797a5790319e9c237b0b6b3f2956304fce5
Alan Gutierrez <alan@prettyrobots.com>
2012-03-30 10:37:16 +0000

    Add bootstrapping to configure.
    Updated `/root/conifgure.sh` to run `/root/boostrap` programs installed from
    `/mnt/config`. There are four possible programs.
    
     * A `common` program, which would be installed for all instances.
     * A program named for the instance type that runs for all instances of that
       type.
     * A program named for `/bin/uname -m` that runs for all instances of the
       machine architecture.
     * A program named `host` that is run for run for the specific host.
    
    These files are copied out of `/mnt/config` using `rsync`.
    
    The `bin/image/clean` script has been updated to recursively delete
    `/root/bootstra`.
    
    Adding machine types to configure.
    
    Adding uname files to configure.
    
    Bootstrap from `/mnt/config`.

--- root/configure.sh
+++ root/configure.sh
@@ -17,10 +17,22 @@ then
     /bin/mount /dev/loop7 /mnt/config
 fi
 
+type=$(echo $hostname | /bin/sed 's/[^.]\+\.\([^.]\+\).*/\1/')
+
 /usr/bin/rsync -av "/mnt/config/var/common/" "/"
+/usr/bin/rsync -av "/mnt/config/var/$(/bin/uname -m)/" "/"
+/usr/bin/rsync -av "/mnt/config/var/$type/" "/"
 /usr/bin/rsync -av "/mnt/config/var/$hostname/" "/"
 
 /bin/hostname $hostname
 /etc/init.d/motd start
+
+for name in common $(/bin/uname -m) $type host
+do
+  if [ -x "/root/bootstrap/$name" ]
+  then
+    "/root/bootstrap/$name"
+  fi
+done
 
 service sshd restart > /dev/null
