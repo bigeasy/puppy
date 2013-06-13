9069f560f1adbfb8130ac9ada5b8dbbdbaea1384
Alan Gutierrez <alan@prettyrobots.com>
2013-06-13 18:27:29 -0400

    Launch a bootstrap image.
    Launching the image, but I won't feel that it is complete until I've
    extracted the SSH server keys and added them to the meta data.
    
    See #19.

--- generate-key-pair.js
+++ generate-key-pair.js
@@ -2,17 +2,65 @@ var AWS = require('aws-sdk');
 var arguable = require('arguable');
 var cadence = require('cadence');
 
-var key = '__puppy_temporary_key_pair_$$$_' + process.pid + '__';
+var keyName = '__puppy_temporary_key_pair_$$$_' + process.pid + '__';
 
 AWS.config.loadFromPath(process.env.HOME + '/.aws');
 AWS.config.update({region: 'us-east-1'});
 
-var ec2 = new AWS.EC2;
+var instance, pending;
 cadence(function (step, ec2) {
   step(function () {
-    ec2.createKeyPair({ KeyName: key }, step());
+    ec2.createKeyPair({ KeyName: keyName }, step());
   }, function (results) {
+    key = results;
     console.log(results);
+    ec2.runInstances({
+      ImageId: 'ami-6f640c06',
+      MinCount: 1,
+      MaxCount: 1,
+      KeyName: keyName,
+      SecurityGroups: [ 'Puppy Image' ],
+      InstanceType: 't1.micro',
+    }, step());
+  }, pending = function (result) {
+    instance = (result.Instances || result.Reservations[0].Instances)[0];
+    if (instance.State.Name == 'pending') {
+      var next = step(pending);
+      step(function () {
+        setTimeout(step(), 5000);
+      }, function () {
+        ec2.describeInstances({ InstanceIds: [ instance.InstanceId ] }, next);
+      });
+    } else {
+      ec2.createTags({
+        Resources: [ instance.InstanceId ],
+        Tags: [
+          { Key: "Name", Value: "Puppy Bootstrap Image" }
+        ]
+      }, step());
+    }
+  }, function (result) {
+    ec2.createTags({
+      Resources: [ instance.BlockDeviceMappings[0].Ebs.VolumeId ],
+      Tags: [
+        { Key: "Name", Value: "Puppy Bootstrap Image" }
+      ]
+    }, step());
+  }, function (result) {
+    var keyMaterial = key.keyMaterial, Attributes = [], count = 0;
+    while (keyMaterial.length) {
+      Attributes.push({
+        Key: "Private Key " + (count++),
+        Value: keyMaterial.substring(0, 255)
+      });
+      keyMaterial = keyMaterial.substring(255);
+    }
+    ec2.createTags({
+      Resources: [ instance.InstanceId ],
+      Tags: Attributes
+    }, step());
+  }, function (result) {
+    console.log(result);
     ec2.describeKeyPairs(step());
   }, function (results) {
     results.KeyPairs.filter(function (pair) {
@@ -22,4 +70,6 @@ cadence(function (step, ec2) {
       ec2.deleteKeyPair({ KeyName: pair.KeyName }, step());
     }));
   });
-})((new AWS.EC2), function () {});
+})((new AWS.EC2), function (error) {
+  if(error) throw error;
+});
