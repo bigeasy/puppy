e96eb4a7d2639fd244f72510384da2aaeffc299d
Alan Gutierrez <alan@prettyrobots.com>
2013-06-18 23:07:07 -0400

    Open ports using un-deprecated API.
    The documentation for `authorizeSecurityGroupIngress` had a typo, or
    rather, a formato, that garbled the description of the contents of
    the `IpPermissions` map property, so I used the deprecated properties.
    This updates the call to `authorizeSecurityGroupIngress` with a correct
    `IpPermissions` map property.
    
    See: aws/aws-sdk-js#119

--- open-ports.js
+++ open-ports.js
@@ -1,8 +1,5 @@
 var AWS = require('aws-sdk');
-var arguable = require('arguable');
 var cadence = require('cadence');
-
-var key = '__puppy_temporary_key_pair_$$$_' + process.pid + '__';
 
 AWS.config.loadFromPath(process.env.HOME + '/.aws');
 AWS.config.update({region: 'us-east-1'});
@@ -11,17 +8,19 @@ var ec2 = new AWS.EC2;
 cadence(function (step, ec2) {
   step(function () {
     ec2.createSecurityGroup({
-      GroupName: "Puppy Image", 
+      GroupName: "Puppy Image",
       Description: "Puppy bootstrap image security group."
     }, step());
   }, function (results) {
     console.log(results);
     ec2.authorizeSecurityGroupIngress({
-      GroupName: "Puppy Image", 
-      IpProtocol: "tcp",
-      CidrIp: "0.0.0.0/0",
-      FromPort: 22,
-      ToPort: 22
+      GroupName: "Puppy Image",
+      IpPermissions: [{
+        IpProtocol: "tcp",
+        IpRanges: [{CidrIp: "0.0.0.0/0"}],
+        FromPort: 22,
+        ToPort: 22
+      }]
     }, step());
   }, function (results) {
     console.log(results);
